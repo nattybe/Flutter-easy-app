756c892aa98e4818f653ccb6dee392df2882aab9
natty <themanintheomoon@gmail.com>
2022-07-25 17:04:40 +0300

    final vesion

--- surafel/lib/main.dart
+++ surafel/lib/main.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       title: "Wheather app",
       home: Home(),
+      debugShowCheckedModeBanner: false,
     ));
 
 class Home extends StatefulWidget {
@@ -34,11 +35,11 @@ class _HomeState extends State<Home> {
     http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=Addis%20Ababa&appid=52321e912f45bad5824de290bd1a0647'
 ));    var results = jsonDecode(response.body);
     setState(() {
-      this.temp = results['main']['temp'];
-      this.discription = results['wheather'][0][' discription'];
-      this.currently = results['main'][0]['currently'];
-      this.humudity = results['wheather']['humudity'];
-      this.windspeed = results['main']['speed'];
+      temp = results['main']['temp'];
+      humudity = results['main']['humidity'];
+      currently = results['name'];
+      windspeed = results['wind']['speed'];
+      this.discription = results['weather'][0]['description'];
     });
   }
 
@@ -61,19 +62,10 @@ class _HomeState extends State<Home> {
           width: MediaQuery.of(context).size.width,
           color: Color.fromARGB(255, 20, 173, 219),
           child: Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 10.0),
-              child: Text(
-                "currently in Addis Abeba",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600),
-              ),
-            ),
+            
             Text(
               // ignore: prefer_interpolation_to_compose_strings
-              temp != null ? temp.toString() + "\u00B0" : "loading",
+              temp != null ? temp.toString() + "\u00B0" : "loading1",
               style: TextStyle(
                   color: Color.fromARGB(255, 54, 53, 53),
                   fontSize: 40.0,
@@ -82,7 +74,7 @@ class _HomeState extends State<Home> {
             Padding(
               padding: EdgeInsets.only(top: 10.0),
               child: Text(
-                currently != null ? currently.toString() : "Loading",
+                currently != null ? currently.toString() : "Loading2",
                 style: TextStyle(
                     color: Color.fromARGB(255, 85, 15, 15),
                     fontSize: 14.0,
@@ -99,31 +91,32 @@ class _HomeState extends State<Home> {
               ListTile(
                 // ignore: deprecated_member_use
                 leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                
                 title: Text("Temprature"),
                 // ignore: prefer_interpolation_to_compose_strings
                 trailing:
-                    Text(temp != null ? temp.toString() + "\u00B0" : "Loading"),
+                    Text(temp != null ? temp.toString() + "\u00B0" : "Loading3"),
               ),
               ListTile(
                 // ignore: deprecated_member_use
                 leading: FaIcon(FontAwesomeIcons.cloud),
                 title: Text("wheather"),
                 trailing: Text(
-                    discription != null ? discription.toString() : "Loading"),
+                    discription != null ? discription.toString() : "Loading4"),
               ),
               ListTile(
                 // ignore: deprecated_member_use
                 leading: FaIcon(FontAwesomeIcons.sun),
                 title: Text("humudity"),
                 trailing:
-                    Text(humudity != null ? humudity.toString() : "Loading"),
+                    Text(humudity != null ? humudity.toString() : "Loading5"),
               ),
               ListTile(
                 // ignore: deprecated_member_use
                 leading: FaIcon(FontAwesomeIcons.wind),
                 title: Text("wind speed"),
                 trailing:
-                    Text(windspeed != null ? windspeed.toString() : "Loading"),
+                    Text(windspeed != null ? windspeed.toString() : "Loading6"),
               ),
             ],
           ),
